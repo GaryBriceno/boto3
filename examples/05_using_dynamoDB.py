6f3f6e9f35f3c0a01fd9a2d0af41dd94215f32a8
Gary Briceño <gary.briceno@gmail.com>
2020-07-04 09:05:38 -0500

    Create 05_using_dynamoDB.py
    Uso del servicio DynamoDB de AWS.

---
+++ examples/05_using_dynamoDB.py
@@ -0,0 +1,43 @@
+import boto3
+
+# Let's use Amazon S3
+session = boto3.session.Session(profile_name='club')
+
+# Get the service resource.
+dynamodb = session.resource('dynamodb')
+
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='users')
+
+# Print out some data about the table.
+print(table.item_count)
